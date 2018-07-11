e7a35283c767e198810028dcc1f9a5562c7e8a64
madclicker88 <madclicker88@gmail.com>
2018-07-11 20:29:29 +0300

    even a little bit optimized

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import { welcome, name as userName } from './brain-games';
 
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import { getName, greeting } from '..';
 
-export const welcome = () => console.log('Welcome to the Brain Games! \nAnswer "yes" if number even otherwise answer "no".');
+export const welcome = () => console.log('Welcome to the Brain Games! \n'
+  + 'Answer "yes" if number even otherwise answer "no".');
 welcome();
 export const name = getName();
 greeting(name);
