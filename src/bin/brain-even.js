4de6e3725cf313a124fb837eea70e199dc7cbd0b
madclicker88 <madclicker88@gmail.com>
2018-07-11 21:02:24 +0300

    even refactored

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,40 +1,10 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { welcome, greeting, getName } from '..';
-
-const QUESTIONS_SIZE = 3;
-
-const answerEvenText = () => console.log('Answer "yes" if number even otherwise answer "no".');
-
-const evenQuizz = (name) => {
-  const randomNum = () => parseInt(Math.random() * 100, 10);
-
-  const worker = (num, questSize) => {
-    if (questSize === 0) {
-      console.log(`Congratulations,  ${name}!`);
-      return;
-    }
-    const userAnswer = readlineSync.question(`Question: ${num} \nYour answer: `);
-    const answerValue = (userAnswer === 'yes');
-    const isEven = !(num % 2);
-    const wrongAnswerMsg = () => console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven}'. \nLet's try again, ${name}!`);
-    if (userAnswer !== 'yes' && userAnswer !== 'no') {
-      wrongAnswerMsg();
-      return;
-    }
-    if (answerValue !== isEven) {
-      wrongAnswerMsg();
-    } else {
-      console.log('Correct!');
-      worker(randomNum(), questSize - 1);
-    }
-  };
-  worker(randomNum(), QUESTIONS_SIZE);
-};
-
+import {
+  welcome, greeting, getName, answerEvenText, evenQuiz,
+} from '..';
 
 welcome();
 answerEvenText();
 const userName = getName();
 greeting(userName);
-evenQuizz(userName);
+evenQuiz(userName);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import readlineSync from 'readline-sync';
 
+const QUESTIONS_SIZE = 3;
+
 export const welcome = () => console.log('Welcome to the Brain Games! ');
 
 export const getName = () => readlineSync.question('May I have your name? ');
 
 export const greeting = (name) => {
   console.log(`Hello, ${name}!\n`);
+};
+
+export const answerEvenText = () => console.log('Answer "yes" if number even otherwise answer "no".');
+
+export const evenQuiz = (name) => {
+  const randomNum = () => parseInt(Math.random() * 100, 10);
+
+  const worker = (num, questSize) => {
+    if (questSize === 0) {
+      console.log(`Congratulations,  ${name}!`);
+      return;
+    }
+    const userAnswer = readlineSync.question(`Question: ${num} \nYour answer: `);
+    const answerValue = (userAnswer === 'yes');
+    const isEven = !(num % 2);
+    const wrongAnswerMsg = () => console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${isEven}'. \nLet's try again, ${name}!`);
+    if (userAnswer !== 'yes' && userAnswer !== 'no') {
+      wrongAnswerMsg();
+      return;
+    }
+    if (answerValue !== isEven) {
+      wrongAnswerMsg();
+    } else {
+      console.log('Correct!');
+      worker(randomNum(), questSize - 1);
+    }
+  };
+  worker(randomNum(), QUESTIONS_SIZE);
 };
