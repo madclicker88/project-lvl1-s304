f01b994c8d33423d26f5fcf0ee3241c4f5da5d58
madclicker88 <madclicker88@gmail.com>
2018-07-13 08:21:02 +0300

    balance added

---
+++ src/games/balance.js
@@ -0,0 +1,36 @@
+import { randomNum } from '../utils';
+import engine from '..';
+
+const MIN = 100;
+const MAX = 10000;
+const rule = 'Balance the given number.';
+
+
+const toArray = n => n.toString().split('').map(i => parseInt(i, 10));
+const toNumber = arr => arr.sort().join('');
+
+const balance = (number) => {
+  const iter = (digits) => {
+    const minDigit = Math.min(...digits);
+    const maxDigit = Math.max(...digits);
+    if (maxDigit - minDigit <= 1) return digits;
+
+    const tempDigitsArray = digits.map((digit) => {
+      if (digit === minDigit) return digit + 1;
+      if (digit === maxDigit) return digit - 1;
+      return digit;
+    });
+    return iter(tempDigitsArray);
+  };
+  return toNumber(iter(toArray(number)));
+};
+
+const game = () => {
+  const num = randomNum(MAX, MIN);
+  return {
+    question: num,
+    answer: balance(num),
+  };
+};
+
+export default () => engine(game, rule);
