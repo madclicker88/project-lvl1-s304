e88cf5d7138cb7587750629d92d589a585a5f2fe
madclicker88 <madclicker88@gmail.com>
2018-07-15 08:33:12 +0300

    progression refactored

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,24 +1,20 @@
 import { randomNum } from '../utils';
 import engine from '..';
 
-const PROGRESSION_LENGTH = 10;
+const progressionLength = 10;
 
 const rule = 'What number is missing in this progression?';
 const hideNumber = (progression, i) => progression.map((value, index) => (index === i ? '..' : value));
 
-const makeProgression = () => {
-  const PROGRESSION_STEP = randomNum(10, 1);
-  const PROGRESSION_START = randomNum(50);
-  const iter = (count = 0, i = PROGRESSION_START, progression = []) => {
-    if (count === PROGRESSION_LENGTH) return progression;
-    progression.push(i);
-    return iter(count + 1, i + PROGRESSION_STEP, progression);
-  };
-  return iter();
-};
+const makeProgression = (size, start, step) => Array(size)
+  .fill(null)
+  .map((_, index) => start + index * step);
 
 const game = () => {
-  const progressionArray = makeProgression();
+  const progressionStep = randomNum(10, 1);
+  const progressionStart = randomNum(50);
+
+  const progressionArray = makeProgression(progressionLength, progressionStart, progressionStep);
   const indexToHide = randomNum(9, 0);
   return {
     question: hideNumber(progressionArray, indexToHide).join(' '),
